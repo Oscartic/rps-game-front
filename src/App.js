9e24d2d688b153b6a1438f23dcc6eb11bc5d0a5f
OscarB <oscartic@gmail.com>
2019-09-07 20:47:49 -0400

    Config state hooks (namePlayers, error, case view) for App and RegisterPlayer Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import RegisterPlayers from './components/RegisterPlayers';
 
 function App() {
+  
+  const [view, setView] = useState(1);
+  const [playerOne, setPlayerOne] = useState("");
+  const [playerTwo, setPlayerTwo] = useState("");
+  const [registerForm, setRegisterForm] = useState(true); 
+
+  
+  const handleViews = (view) => {
+    switch (view){
+      case 1: 
+        return <RegisterPlayers setPlayerOne={ setPlayerOne } setPlayerTwo={ setPlayerTwo } setRegisterForm={ setRegisterForm } setView={ setView } />
+      case 2: 
+        return "A Jugar"
+      default:
+        return <RegisterPlayers setPlayerOne={ setPlayerOne } setPlayerTwo={ setPlayerTwo } setRegisterForm={ setRegisterForm } />
+    }
+  }
+
+
   return (
     <div className="App">
       <h1>CachiPum!</h1>
-      <RegisterPlayers/>
+      { handleViews(view) }
     </div>
   );
 }
--- src/components/RegisterPlayers.js
+++ src/components/RegisterPlayers.js
@@ -1,23 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Input, Button } from 'semantic-ui-react';
-function RegisterPlayers(){
+
+function RegisterPlayers(props){
+
+    const { setPlayerOne, setPlayerTwo, setRegisterForm, setView } = props;
+
+    const [nickNameP1, setNickNameP1] = useState("");
+    const [nickNameP2, setNickNameP2] = useState("");
+    const [error, setError] = useState(false);
+    const handleSubmit = () => {
+        // Validations
+        if(nickNameP1.length <= 2 && nickNameP2.length <= 2){
+            setError(true);
+            return;
+        }
+        if(nickNameP1 === nickNameP2){
+            setError(true);   
+            return;
+        }
+
+        setError(false);
+        setPlayerOne(nickNameP1);
+        setPlayerTwo(nickNameP2);
+        setRegisterForm(false);
+        setView(2);
+
+    }
 
     return(
         <div>
             <h2>Ingresar Nombre de los jugadores</h2>
-            <Form>
+            {error ? <div>Cada nickName debe tener mas de dos caracteres y deben ser diferentes</div> : ""}
+            <Form onSubmit={handleSubmit}>
                 <Form.Group widths='equal'>
-                    <Form.Field
+                    <Form.Field 
                         control={Input}
                         label='Player 1'
-                        placeholder='P1'
-                        //onChange={}
+                        placeholder='Nick Name P1'
+                        value={nickNameP1}
+                        onChange={e => setNickNameP1(e.target.value)}
                     />
                     <Form.Field
                         control={Input}
                         label='Player 2'
-                        placeholder='P2'
-                        //onChange={}
+                        value={nickNameP2}
+                        placeholder='Nick Name P2'
+                        onChange={e => setNickNameP2(e.target.value)}
                     />
                 </Form.Group>
                 <Form.Field control={Button}>Start</Form.Field>
