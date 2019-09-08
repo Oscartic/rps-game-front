fc489a36811c9626939f0319b8356a89ad85f789
OscarB <oscartic@gmail.com>
2019-09-08 17:36:07 -0300

    Config useEffects, added bg-image for each player round

--- src/components/Round.js
+++ src/components/Round.js
@@ -1,15 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Button } from 'semantic-ui-react';
 
 function Round(props){
     
-    const { playerOne, playerTwo, setView } = props;
+    const { players, setView } = props;
 
-    
     const [round, setRound] = useState(1);
+    const [playersCreator, setPlayersCreator] = useState(players);
     const [moves, setMoves] = useState([{move: 'paper', kills: 'rock'}, {move: 'rock', kills: 'scissors'}, {move: 'scissors', kills: 'paper'}]);
-    const [p1, setP1] = useState({ nick: playerOne, score: 0, currentMove: "", currentKills: "none1" });
-    const [p2, setP2] = useState({ nick: playerTwo, score: 0, currentMove: "", currentKills: "none2" });
+    const [p1, setP1] = useState({ id: 0, nick: "", wins: 0, currentMove: "", currentKills: "none1" });
+    const [p2, setP2] = useState({ id: 0, nick: "", wins: 0, currentMove: "", currentKills: "none2" });
     const [turn, setTurn] = useState(false);
+    const [currentSelectAttack, setCurrentSelectAttack] = useState(false);
+
+    useEffect(() => {
+        if(players.length && p1.id === 0){
+            setP1({id: players[0][0].id, nick: players[0][0].name, wins: players[0][0].wins, currentMove: "", currentKills: "none1"})
+        }
+        if(players.length && p2.id === 0){
+            setP1({id: players[0][1].id, nick: players[0][1].name, wins: players[0][1].wins, currentMove: "", currentKills: "none1"})
+        }         
+    }, [players, p1, p2]);
 
     const validateWinRound = () => {
         if(p1.score === 3){
@@ -25,23 +36,30 @@ function Round(props){
         
     }
     const handleTurn = (move, kills) => {
-        let currentRound = round;
-        setTurn(!turn);
+        setCurrentSelectAttack(!currentSelectAttack);
         if(turn){
             setP2({...p2, currentMove: move, currentKills: kills});
         } else {
             setP1({...p1, currentMove: move, currentKills: kills});
-            if(p1.currentKills === p2.currentMove){
-                let newPoint = p1.score + 1;
-                setP1({...p1, score: newPoint})
-                setRound(currentRound + 1);
-            } else if(p2.currentKills === p1.currentMove) {
-                let newPoint = p2.score + 1;
-                setP2({...p2, score: newPoint})
-            }
-        }     
-        validateWinRound();
+        }        
     } 
+
+    const handleLaunchAttack = () => {
+        let currentRound = round;
+        setTurn(!turn);
+        if(p1.currentKills === p2.currentMove){
+            let newPoint = p1.score + 1;
+            setP1({...p1, score: newPoint, currentKills: 'none1'})
+            setRound(currentRound + 1);
+            validateWinRound();
+        } else if(p2.currentKills === p1.currentMove) {
+            let newPoint = p2.score + 1;
+            setP2({...p2, score: newPoint, currentKills: 'none2'})
+            setRound(currentRound + 1);
+            validateWinRound();
+        }
+    }
+
     return(
         <div className={ turn ? 'turn-player-two' : 'turn-player-one'}>
             <h1>Round { round }</h1>
@@ -49,21 +67,28 @@ function Round(props){
                 
             <div className="all-moves">
                 {moves.map((move)=>(
-                    <div key={move.move} className="move" onClick={() => handleTurn(move.move, move.kills)}>{move.move}</div>
+                    <div key={move.move} className={ currentSelectAttack ? 'move selected-move' : 'move'} onClick={() => handleTurn(move.move, move.kills)}>{move.move}</div>
                 ))}
             </div>
+            
+            <div className="send-attack-zone">
+                <Button fluid className="btn-turn" color="orange" onClick={() => handleLaunchAttack()}>Launch attack</Button>
+            </div>
+
+
             <div className="score">
                     <div className="p1-score">
                         <h2>Player One</h2>
-                        <p>{p1.score}</p>
+                        <p>{ p1.wins }</p>
                         <strong>W on games</strong>
                     </div>
                     <div className="p2-score">
                         <h2>Player Two</h2>
-                        <p>{p2.score}</p>
+                        <p>{ p2.wins }</p>
                         <strong>Won games</strong>
                     </div>
             </div>
+            
         </div>
     );
 }
