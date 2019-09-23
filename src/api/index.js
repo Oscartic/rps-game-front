cfc52674c016ee675b588aab28424a8959f0bce2
OscarB <oballesteros@abstract.cl>
2019-09-22 22:50:35 -0300

    added urlBase production server

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,7 @@
 import Axios from "axios";
 
-const baseUrl =  'http://localhost:3000/api/v1';
+//const baseUrl =  'http://localhost:3000/api/v1';
+const baseUrl =  'https://rps-game-api.herokuapp.com/api/v1';
 
 export function postPlayers(object) {
     return Axios.post(baseUrl + '/players/create_round_players', object)
